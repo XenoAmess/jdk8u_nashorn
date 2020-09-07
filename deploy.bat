76a601ea1722af0c303ddefced2f9342be3d6d5f
XenoAmess <xenoamess@gmail.com>
2020-09-07 12:43:02 +0800

    init

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
 call mvn -Possrh deploy
 call mvn -Possrh org.apache.maven.plugins:maven-gpg-plugin:1.6:sign-and-deploy-file -DpomFile=pom.xml -Dfile=nashorn.jar -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/
+call mvn install:install-file -DpomFile=pom.xml -Dfile=nashorn.jar -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -Possrh
 pause
